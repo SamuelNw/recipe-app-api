1e70e84d5c84df9fcd806bdbdaf24e3efd5b26b3
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-04-07 11:45:15 +0300

    Code to handle db race condition

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,6 +2,11 @@
 Custom django management command.
 """
 
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -9,4 +14,16 @@ class Command(BaseCommand):
     """Custom command to wait for the db to be ready."""
 
     def handle(self, *args, **options):
-        pass
+        """Command entrypoint."""
+        self.stdout.write("Waiting for database...")
+        db_up = False
+
+        while db_up == False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database is ready!'))
