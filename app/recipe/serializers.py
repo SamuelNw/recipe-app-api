8dcb13e5427cd0ab940f353a8582a9124fb270ca
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-04-18 11:58:25 +0300

    Implement listing API calls

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,7 +4,13 @@ Recipe serializer
 
 from rest_framework import serializers
 
+from core.models import Recipe
+
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for the recipe obj."""
-    pass
+
+    class Meta:
+        model = Recipe
+        fields = ["id", "title", "price", "time_in_minutes", "link"]
+        read_only_fields = ["id"]
