b20aa173934369c06ca687f4ce6e8ee6829cdd7a
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-04-18 17:42:39 +0300

    Implement tags listing api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,11 +2,14 @@
 Views for the recipe APIs.
 """
 
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (Recipe, Tag)
 from recipe import serializers
 
 
@@ -31,3 +34,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Save the authenticated user."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """View for managing tags APIs."""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Overide method to filter tags to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
