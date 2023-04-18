e5b791bfc755b56785cc54efdf4aabb1b943f521
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-04-18 13:28:52 +0300

    Refactor

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -43,6 +43,11 @@ def create_recipe(user, **params):
     return recipe
 
 
+def create_user(**params):
+    """Helper function to create a new user."""
+    return get_user_model().objects.create_user(**params)
+
+
 class PublicRecipeAPITests(TestCase):
     """Test unauthenticated requests to the api."""
 
@@ -62,10 +67,8 @@ class PrivateRecipeAPITests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
-            "test@example.com",
-            "testpass123"
-        )
+        self.user = create_user(
+            email="test@example.com", password="testpass123")
         self.client.force_authenticate(self.user)
 
     def test_retrieve_recipes(self):
@@ -82,10 +85,8 @@ class PrivateRecipeAPITests(TestCase):
 
     def test_recipe_list_limited_to_user(self):
         """Test list of recipes is limited to authenticated user."""
-        other_user = get_user_model().objects.create_user(
-            "otheruser@example.com",
-            "testpassword123"
-        )
+        other_user = create_user(
+            email="otheruser@example.com", password="testpassword123")
 
         create_recipe(user=other_user)
         create_recipe(user=self.user)
