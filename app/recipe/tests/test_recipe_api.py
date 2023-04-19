06aa1b5ac8d542f486be12233f09c529e8656778
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-04-19 18:27:03 +0300

    Add tests for creating recipes with new ingredients

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -14,6 +14,7 @@ from rest_framework.test import APIClient
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 
 from recipe.serializers import (
@@ -300,3 +301,26 @@ class PrivateRecipeAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.tags.count(), 0)
+
+    def test_create_recipe_with_new_ingredients(self):
+        """Test creating new recipe with new ingredients."""
+        payload = {
+            "title": "Vietnamese soup.",
+            "time_in_minutes": 60,
+            "price": Decimal("2.50"),
+            "ingredients": [{"name": "fish"}, {"name": "lemon"}]
+        }
+
+        res = self.client.post(RECIPES_URL, payload, format="json")
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        recipes = Recipe.objects.filter(user=self.user)
+        self.assertEqual(recipes.count(), 1)
+        recipe = recipes[0]
+        self.assertEqual(recipe.ingredients.count(), 2)
+        for ingredient in payload["ingredients"]:
+            exists = recipe.ingredients.filter(
+                user=self.user,
+                name=ingredient["name"]
+            ).exists()
+            self.assertTrue(exists)
