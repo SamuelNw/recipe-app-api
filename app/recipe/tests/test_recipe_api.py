e18f91400493ded31e41f73f8f4bf40dc1a90df2
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-04-26 15:23:24 +0300

    Tests for filtering recipes by ingredients

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -413,7 +413,7 @@ class PrivateRecipeAPITests(TestCase):
         self.assertEqual(recipe.ingredients.count(), 0)
 
     def test_filtering_by_tags(self):
-        """Test filtering recipes with tags."""
+        """Test filtering recipes by tags."""
         r1 = create_recipe(user=self.user, title="Vegetable Curry")
         r2 = create_recipe(user=self.user, title="Cabbage Soup")
         tag1 = Tag.objects.create(user=self.user, name="vegetarian")
@@ -423,6 +423,26 @@ class PrivateRecipeAPITests(TestCase):
         r3 = create_recipe(user=self.user, title="Beef Stew")
 
         params = {"tags": f"{tag1.id}, {tag2.id}"}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
+    def test_filtering_by_ingredients(self):
+        """Test filtering recipes by ingredients."""
+        r1 = create_recipe(user=self.user, title="Chicken Stew")
+        r2 = create_recipe(user=self.user, title="Pizza")
+        in1 = Ingredient.objects.create(user=self.user, name="chicken")
+        in2 = Ingredient.objects.create(user=self.user, name="flour")
+        r1.ingredients.add(in1)
+        r2.ingredients.add(in2)
+        r3 = create_recipe(user=self.user, title="Beef Stew")
+
+        params = {"ingredients": f"{in1.id}, {in2.id}"}
         res = self.client.get(RECIPES_URL, params)
 
         s1 = RecipeSerializer(r1)
