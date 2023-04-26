bad2ba13c9cfc1bddfa9d8b943ba9171be7602a4
Ng'ash_254 <samuelnganga.w@gmail.com>
2023-04-26 18:45:11 +0300

    Fix linting errors

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -122,7 +122,8 @@ class BaseRecipeAttrViewSet(
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
 
-        return queryset.filter(user=self.request.user).order_by("-name").distinct()
+        return queryset.filter(
+            user=self.request.user).order_by("-name").distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
